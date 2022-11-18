4c56a65a0572be0e0378d34080e601082e3390cf
Razia Sultana <Razia Sultana@Razia.lan>
2022-11-17 19:01:08 -0500

    This is first push to github

---
+++ batch31smart_tech/src/main/java/batch31smart_tech/Ayaan.java
@@ -0,0 +1,16 @@
+package batch31smart_tech;
+
+public class Ayaan {
+void cat() {
+System.out.println(" My cat name is pusy");}
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Ayaan obj =new Ayaan ();
+	obj.cat();
+		
+	}
+
+}
